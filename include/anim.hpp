3e1ba4489a8a9d0f7bbaa0088b6b3a66c5df364d
kseny <cristeastefan1999@gmail.com>
2022-04-01 02:19:51 +0300

    Added animationLibraryValid

--- include/anim.hpp
+++ include/anim.hpp
@@ -1819,6 +1819,36 @@ static StringView AnimationNames[] = {
     "SAMP:FISHINGIDLE",
 };
 
+static StringView AnimLibs[] = {
+    "AIRPORT", "ATTRACTORS", "BAR", "BASEBALL", "BD_FIRE",
+    "BEACH", "BENCHPRESS", "BF_INJECTION", "BIKE_DBZ", "BIKED",
+    "BIKEH", "BIKELEAP", "BIKES", "BIKEV", "BLOWJOBZ",
+    "BMX", "BOMBER", "BOX", "BSKTBALL", "BUDDY",
+    "BUS", "CAMERA", "CAR", "CAR_CHAT", "CARRY",
+    "CASINO", "CHAINSAW", "CHOPPA", "CLOTHES", "COACH",
+    "COLT45", "COP_AMBIENT", "COP_DVBYZ", "CRACK", "CRIB",
+    "DAM_JUMP", "DANCING", "DEALER", "DILDO", "DODGE",
+    "DOZER", "DRIVEBYS", "FAT", "FIGHT_B", "FIGHT_C",
+    "FIGHT_D", "FIGHT_E", "FINALE", "FINALE2", "FLAME",
+    "FLOWERS", "FOOD", "FREEWEIGHTS", "GANGS", "GFUNK",
+    "GHANDS", "GHETTO_DB", "GOGGLES", "GRAFFITI", "GRAVEYARD",
+    "GRENADE", "GYMNASIUM", "HAIRCUTS", "HEIST9", "INT_HOUSE",
+    "INT_OFFICE", "INT_SHOP", "JST_BUISNESS", "KART", "KISSING",
+    "KNIFE", "LAPDAN1", "LAPDAN2", "LAPDAN3", "LOWRIDER",
+    "MD_CHASE", "MD_END", "MEDIC", "MISC", "MTB",
+    "MUSCULAR", "NEVADA", "ON_LOOKERS", "OTB", "PARACHUTE",
+    "PARK", "PAULNMAC", "PED", "PLAYER_DVBYS", "PLAYIDLES",
+    "POLICE", "POOL", "POOR", "PYTHON", "QUAD",
+    "QUAD_DBZ", "RAPPING", "RIFLE", "RIOT", "ROB_BANK",
+    "ROCKET", "RUNNINGMAN", "RUSTLER", "RYDER", "SAMP",
+    "SCRATCHING", "SEX", "SHAMAL", "SHOP", "SHOTGUN",
+    "SILENCED", "SKATE", "SMOKING", "SNIPER", "SNM",
+    "SPRAYCAN", "STRIP", "SUNBATHE", "SWAT", "SWEET",
+    "SWIM", "SWORD", "TANK", "TATTOOS", "TEC",
+    "TRAIN", "TRUCK", "UZI", "VAN", "VENDING",
+    "VORTEX", "WAYFARER", "WEAPONS", "WOP", "WUZI"
+};
+
 /// Quick animation validity lookup
 static struct AnimationLookup {
     FlatHashSet<Impl::String> valid;
@@ -1829,6 +1859,18 @@ static struct AnimationLookup {
         }
     }
 } AnimationNamesLookup;
+
+
+static struct AnimationLibraryLookup {
+    FlatHashSet<Impl::String> valid;
+
+    AnimationLibraryLookup()
+    {
+        for (int i = 0; i < GLM_COUNTOF(AnimLibs); ++i) {
+            valid.emplace(Impl::String(AnimLibs[i]));
+        }
+    }
+} AnimationLibraryLookup;
 
 inline Pair<StringView, StringView> splitAnimationNames(int ID)
 {
@@ -1850,6 +1892,13 @@ inline bool animationNameValid(StringView lib, StringView name) {
     fullName += ':' + Impl::String(name);
     std::transform(fullName.begin(), fullName.end(), fullName.begin(), [](unsigned char c) { return std::toupper(c); });
     return AnimationNamesLookup.valid.find(fullName) != AnimationNamesLookup.valid.end();
+}
+
+inline bool animationLibraryValid(StringView lib)
+{
+    Impl::String libString(lib);
+    std::transform(libString.begin(), libString.end(), libString.begin(), [](unsigned char c) { return std::toupper(c); });
+    return AnimationLibraryLookup.valid.find(libString) != AnimationLibraryLookup.valid.end();
 }
 
 /* Interfaces, to be passed around */
