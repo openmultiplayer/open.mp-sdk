c74a3828dd1c09334ceca2b3d5be9e2bee4d68a7
Y_Less <git-athena@y-less.com>
2022-04-01 18:18:30 +0100

    `hide()` dialog method.
    # Conflicts:
    #	include/Server/Components/Dialogs/dialogs.hpp

--- include/Server/Components/Dialogs/dialogs.hpp
+++ include/Server/Components/Dialogs/dialogs.hpp
@@ -21,10 +21,10 @@ static const UID DialogData_UID = UID(0xbc03376aa3591a11);
 struct IPlayerDialogData : public IExtension {
     PROVIDE_EXT_UID(DialogData_UID);
 
-    /// Hide a dialog for a player
+    /// Hide a dialog from a player
     virtual void hide(IPlayer& player) = 0;
-
-    /// Show a dialog to player
+	
+    /// Show a dialog to a player
     virtual void show(IPlayer& player, int id, DialogStyle style, StringView title, StringView body, StringView button1, StringView button2) = 0;
 
 	/// Get a player's current dialog data.
