9b399bf5843fa4354e00df309aaa0ea2bf4e9cf9
open.mp <open.mp@github.com>
2024-01-29 01:34:41 +0000

    Update SDK
    with https://github.com/openmultiplayer/open.mp/commit/41ebf182bf73dc9cb11841bfe6b0179ba31f9dd5

--- include/utils.hpp
+++ include/utils.hpp
@@ -1,4 +1,5 @@
 #pragma once
+#include "player.hpp"
 
 #define STRINGIFY(s) _STRINGIFY(s)
 #define _STRINGIFY(s) #s
@@ -13,4 +14,21 @@ inline StringView trim(StringView view)
 	}
 	const size_t end = view.find_last_not_of(whitespace);
 	return view.substr(start, end - start + 1);
+}
+
+inline bool IsWeaponForTakenDamageValid(int weapon)
+{
+	auto slot = WeaponSlotData(weapon).slot();
+	if (slot == INVALID_WEAPON_SLOT)
+	{
+		// 52 is a valid damage reason, though not a valid death reason.  It is given by the police boat gun.
+		//
+		//   https://github.com/openmultiplayer/open.mp/issues/600#issuecomment-1377236916
+		//
+		if (weapon < 49 || weapon > 54)
+		{
+			return false;
+		}
+	}
+	return true;
 }
