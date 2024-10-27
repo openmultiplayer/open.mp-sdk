72362f77321c32b15811014b81165a38ebee9d61
Vladimir <68227685+NoPressF@users.noreply.github.com>
2024-10-27 11:14:28 +0100

    Added timer's virtual functions (set interval & pause/resume)

--- include/Server/Components/Timers/timers.hpp
+++ include/Server/Components/Timers/timers.hpp
@@ -12,6 +12,9 @@ struct ITimer : public IExtensible
 	/// Get whether the timer is running or has been killed
 	virtual bool running() const = 0;
 
+	/// Get the timer paused state
+	virtual bool paused() const = 0;
+
 	/// Get the remaining time until time out
 	virtual Milliseconds remaining() const = 0;
 
@@ -21,9 +24,15 @@ struct ITimer : public IExtensible
 	/// Get the timer's interval
 	virtual Milliseconds interval() const = 0;
 
+	/// Set the timer's interval
+	virtual void setInterval(Milliseconds interval) = 0;
+
 	/// Mark the timer as being called now.  Returns `true` when there are more
 	/// calls to make after this one.
 	virtual bool trigger() = 0;
+
+	/// Toggle the timer paused state
+	virtual void togglePause(bool paused) = 0;
 
 	/// Immediately kill the timer
 	virtual void kill() = 0;
